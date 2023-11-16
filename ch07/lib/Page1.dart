a5c153ecb34a0278c7a64c18d1c91a90edf40b11
KimJH3475 <110102101+KimJH3475@users.noreply.github.com>
2023-11-16 17:40:37 +0900

    11.16

--- ch07/lib/Page1.dart
+++ ch07/lib/Page1.dart
@@ -5,11 +5,121 @@ class Page1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        '홈페이지 1',
-        style: TextStyle(fontSize: 40),
-      ),
+    return Column(
+      children: [
+        _buildTop(),
+        _buildMiddle(),
+        _buildBottom(),
+      ],
     );
+  }
+
+  //메뉴
+  Widget _buildTop() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            GestureDetector(
+              onTap: () {
+                print('클릭');
+              },
+              child: Column(
+                children: [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('블랙'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('바이크'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('대리'),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('택시'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('블랙'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('바이크'),
+              ],
+            ),
+            Opacity(
+              opacity: 0.0,
+              child: Column(
+                children: [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('대리'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMiddle() {
+    return Text('Middle');
+  }
+
+  Widget _buildBottom() {
+    return Text('Bottom');
   }
 }
